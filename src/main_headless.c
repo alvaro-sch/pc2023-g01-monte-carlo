15afd94928a16a51e19d54f47c252032bca9cf83
Alvaro Roy Schachner <alvarolegit@gmail.com>
2023-06-02 09:42:05 -0300

    removed useless paramters

--- src/main_headless.c
+++ src/main_headless.c
@@ -13,38 +13,30 @@
 typedef unsigned long ulong;
 
 void usage(const char *prog_name) {
-    fprintf(stderr,
-            "usage: %s <SHELLS> <photons> <MU_A> <MU_S> <MICRONS_PER_SHELL> "
-            "<SEED>\n",
-            prog_name);
+    fprintf(stderr, "usage: %s <photons> <SEED>\n", prog_name);
     exit(1);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 7) {
+    if (argc < 3) {
         usage(argv[0]);
     }
 
-    ulong shells = strtoul(argv[1], NULL, 10);
-    ulong photons = strtoul(argv[2], NULL, 10);
-    ulong seed = strtoul(argv[6], NULL, 10);
-    ulong microns_per_shell = strtoul(argv[5], NULL, 10);
-
-    float mu_a = strtof(argv[3], NULL);
-    float mu_s = strtof(argv[4], NULL);
+    ulong photons = strtoul(argv[1], NULL, 10);
+    ulong seed = strtoul(argv[2], NULL, 10);
 
     srand(seed);
-    uint32_t rand_state[4] = { rand(), rand(), rand(), rand() };
 
     const struct photon_params params = {
-        .microns_per_shell = microns_per_shell,
-        .shells = shells,
-        .mu_a = mu_a,
-        .mu_s = mu_s,
+        .microns_per_shell = MICRONS_PER_SHELL,
+        .shells = SHELLS,
+        .mu_a = MU_A,
+        .mu_s = MU_S,
     };
 
-    float *heats = calloc(shells, sizeof(*heats));
-    float *heats2 = calloc(shells, sizeof(*heats2));
+    uint32_t rand_state[4] = { rand(), rand(), rand(), rand() };
+    float heats[SHELLS] = {0.0};
+    float heats2[SHELLS] = {0.0};
 
     double start = wtime();
     for (unsigned int i = 0; i < photons; ++i) {
@@ -62,21 +54,19 @@ int main(int argc, char *argv[]) {
     printf("# Radius\tHeat\n");
     printf("# [microns]\t[W/cm^3]\tError\n");
 
-    float t = 4.0f * M_PI * powf(microns_per_shell, 3.0f) * photons / 1e12;
+    float t = 4.0f * M_PI * powf(MICRONS_PER_SHELL, 3.0f) * photons / 1e12;
 
-    for (unsigned int i = 0; i < shells - 1; ++i) {
+    for (unsigned int i = 0; i < SHELLS - 1; ++i) {
         float vol = i * i + i + 1.0 / 3.0;
 
-        printf("%6.0f\t%12.5f\t%12.5f\n", i * (float)microns_per_shell,
+        printf("%6.0f\t%12.5f\t%12.5f\n", i * (float)MICRONS_PER_SHELL,
                heats[i] / t / vol,
                sqrt(heats2[i] -
                     heats[i] * heats[i] / photons) /
                    t / vol);
     }
 
-    printf("# extra\t%12.5f\n", heats[shells - 1] / photons);
-
-    free(heats);
+    printf("# extra\t%12.5f\n", heats[SHELLS - 1] / photons);
 
     return 0;
 }
